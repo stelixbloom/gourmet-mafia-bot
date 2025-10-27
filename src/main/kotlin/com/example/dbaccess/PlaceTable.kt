7e193894f181c80e1b0ab97750a8fccc20b8b095
stelixbloom <stelixbloom@gmail.com>
2025-10-27 16:03:12 +0900

    table settings

--- src/main/kotlin/com/example/dbaccess/PlaceTable.kt
+++ src/main/kotlin/com/example/dbaccess/PlaceTable.kt
@@ -2,11 +2,11 @@ package com.example.dbaccess
 
 import org.jetbrains.exposed.sql.Table
 
-object PlaceTable : Table("places") {
+object PlaceTable : Table("tbl_places") {
     val id = text("id")                 // place_id
     val name = text("name")
     val city = text("city")
     val shopUrl = text("shop_url")
-    val active = bool("active_flag").default(true)
+    val defectFlag = bool("defect_flag")
     override val primaryKey = PrimaryKey(id)
 }
