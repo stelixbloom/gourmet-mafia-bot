559a1d049d1fd7440599003918a779295bf3c830
stelixbloom <stelixbloom@gmail.com>
2025-11-16 19:44:46 +0900

    domain sort out

--- src/main/kotlin/com/example/dbaccess/PlaceRepository.kt
+++ src/main/kotlin/com/example/dbaccess/PlaceRepository.kt
@@ -1,12 +1,6 @@
 package com.example.dbaccess
 
-import com.example.domain.model.Place
-import com.example.domain.model.SearchCriteria
 import com.example.domain.port.PlaceQueryPort
-import org.jetbrains.exposed.sql.Op
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
---
+++ src/main/kotlin/com/example/domain/model/HoursBand.kt
@@ -0,0 +1,7 @@
+package com.example.domain.model
+
+enum class HoursBand(val jpWord: String) {
+    MORNING("モーニング"),
+    LUNCH("ランチ"),
+    DINNER("ディナー")
+}
