97a7a9e72f86992e812d717c828ae871d0e26b51
stelixbloom <stelixbloom@gmail.com>
2025-11-17 20:40:41 +0900

    comment edit

--- src/main/kotlin/com/example/dbaccess/PlaceTable.kt
+++ src/main/kotlin/com/example/dbaccess/PlaceTable.kt
@@ -4,8 +4,8 @@ import org.jetbrains.exposed.sql.Table
 
 object PlaceTable : Table("tbl_places") {
     val id = text("id")
-    val name = text("name").nullable()
-    val areaInfo = text("area_info").nullable()
+    val name = text("name").nullable() // アプリ上では使用しないデータの想定（データ一覧見るときの補足情報）
+    val areaInfo = text("area_info").nullable() // アプリ上では使用しないデータの想定（データ一覧見るときの補足情報）
     val rating = decimal("rating", precision = 2, scale = 1).nullable()
     val comment = text("comment").nullable()
     val defectFlag = short("defect_flag").default(0)
