b868b78f17a49670c727ed6e48d636a65f0ca9c8
stelixbloom <stelixbloom@gmail.com>
2025-11-17 20:28:42 +0900

    comment edit

--- src/main/kotlin/com/example/dbaccess/PlaceTable.kt
+++ src/main/kotlin/com/example/dbaccess/PlaceTable.kt
@@ -4,7 +4,9 @@ import org.jetbrains.exposed.sql.Table
 
 object PlaceTable : Table("tbl_places") {
     val id = text("id")
-    val name = text("name")
+    val name = text("name").nullable()
+    val areaInfo = text("area_info").nullable()
+    val rating = text("rating").nullable()
     val comment = text("comment").nullable()
     val defectFlag = short("defect_flag").default(0)
     override val primaryKey = PrimaryKey(id)
